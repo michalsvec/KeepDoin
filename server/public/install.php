77e9718066b6e47aa6d25526015b6609ecd5c633
Jan Javorek <honza@javorek.net>
2010-11-08 14:40:04 +0100

    server: web interface for load script, production fixtures

---
+++ server/public/install.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * THIS FILE HAS TO BE DELETED AFTER INSTALLATION,
+ * OTHERWISE IT IS A HUGE VULNERABILITY
+ */
+
+define('ROOT_DIR', dirname(__FILE__) . '/..');
+require dirname(__FILE__) . '/../data/load.php';
