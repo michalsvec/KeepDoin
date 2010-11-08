f5f94bda1aefcdd511b6f295c21fce933c14c349
Michal Svec <pan.svec@gmail.com>
2010-11-08 17:49:57 +0100

    pridan layout pro seznam friendu

--- client/src/cz/vutbr/fit/tam/and10/FriendsTab.java
+++ client/src/cz/vutbr/fit/tam/and10/FriendsTab.java
@@ -2,14 +2,28 @@ package cz.vutbr.fit.tam.and10;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class FriendsTab extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.friends);
 
-        TextView textview = new TextView(this);
-        textview.setText("Iueta, Miuek, Uoman...");
-        setContentView(textview);
+        GridView gridview = (GridView) findViewById(R.id.friendsview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+
+        // event na kliknuti
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(FriendsTab.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+
     }
 }
