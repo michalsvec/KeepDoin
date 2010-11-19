9f7f1b87692bd1db2d3b9d8cf9e1ff11f7baa1f4
Jan Javorek <honza@javorek.net>
2010-11-19 15:18:27 +0100

    fixes of bugs caused by renaming

--- client/src/cz/vutbr/fit/tam/and10/GameModel.java
+++ client/src/cz/vutbr/fit/tam/and10/GameModel.java
@@ -12,7 +12,6 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
