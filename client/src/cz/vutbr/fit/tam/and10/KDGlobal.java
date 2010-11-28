949c02555b6cb5c8986f6fdac28c3d952ab33374
Michal Svec <pan.svec@gmail.com>
2010-11-28 23:17:39 +0100

    ADD: global class for storing global data removed padding from tab content

---
+++ client/src/cz/vutbr/fit/tam/and10/KDGlobal.java
@@ -0,0 +1,15 @@
+package cz.vutbr.fit.tam.and10;
+
+import android.app.Application;
+
+
+/**
+ * Global class which contains data, we want to share across the application.
+ * 
+ * getting in any activity: KDGlobal global = (KDGlobal) getApplication();
+ * @author misa
+ *
+ */
+public class KDGlobal extends Application {
+
+}
